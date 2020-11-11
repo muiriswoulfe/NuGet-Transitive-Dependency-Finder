37c46d3ba92c48440d6c25dc8e16ef5a7209113d
Muiris Woulfe <mw.github@outlook.com>
2020-11-11 00:08:05 +0000

    Fixing issues

---
+++ src/NuGet.TransitiveDependency.Finder.ConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+// <copyright file="AssemblyInfo.cs" company="Muiris Woulfe">
+// © Muiris Woulfe
+// Licensed under the MIT License
+// </copyright>
+
+using System;
+using System.Runtime.InteropServices;
+
+[assembly:CLSCompliant(true)]
+[assembly:ComVisible(false)]
