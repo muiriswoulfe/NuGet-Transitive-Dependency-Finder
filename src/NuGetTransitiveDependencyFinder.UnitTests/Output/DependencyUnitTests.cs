bd7bdfa975bcebc62f9b7ec03644c63c366f608d
Muiris Woulfe <mw.github@outlook.com>
2020-12-13 19:08:34 +0000

    Significantly more tests, but many fail

--- src/NuGetTransitiveDependencyFinder.UnitTests/Output/DependencyUnitTests.cs
+++ src/NuGetTransitiveDependencyFinder.UnitTests/Output/DependencyUnitTests.cs
@@ -5,6 +5,8 @@
 
 namespace NuGetTransitiveDependencyFinder.UnitTests.Output
 {
+    using System;
+    using System.Collections.Generic;
     using NuGet.Versioning;
     using NuGetTransitiveDependencyFinder.Output;
     using Xunit;
@@ -23,6 +25,77 @@ namespace NuGetTransitiveDependencyFinder.UnitTests.Output
         /// The default version.
         /// </summary>
         private static readonly NuGetVersion DefaultVersion = new NuGetVersion("1.0.0");
+
+        /// <summary>
+        /// The default dependency.
+        /// </summary>
+        private static readonly Dependency DefaultDependency = new Dependency(DefaultIdentifier, DefaultVersion);
+
+        /// <summary>
+        /// The data for testing the operators.
+        /// </summary>
+        private static readonly (
+            Dependency left,
+            Dependency right,
+            bool isEqual,
+            bool isLessThan,
+            bool isGreaterThan)[] OperatorTestData = new[]
+            {
+                (null, null, true, false, false),
+                (null, DefaultDependency, false, false, false),
+                (DefaultDependency, null, false, false, true),
+                (DefaultDependency, DefaultDependency, true, false, false),
+                (DefaultDependency, new Dependency("Identifier", DefaultVersion), true, false, false),
+                (DefaultDependency, new Dependency(DefaultIdentifier, new NuGetVersion("1.0.0")), true, false, false),
+                (DefaultDependency, new Dependency("xyz", DefaultVersion), false, true, false),
+                (DefaultDependency, new Dependency("XYZ", DefaultVersion), false, true, false),
+                (DefaultDependency, new Dependency(DefaultIdentifier, new NuGetVersion("1.0.1")), false, true, false),
+                (DefaultDependency, new Dependency("abc", DefaultVersion), false, false, true),
+                (DefaultDependency, new Dependency("ABC", DefaultVersion), false, false, true),
+                (DefaultDependency, new Dependency(DefaultIdentifier, new NuGetVersion("0.9.9")), false, false, true),
+            };
+
+        /// <summary>
+        /// Generates the data for testing <see cref="Dependency.operator =="/>.
+        /// </summary>
+        /// <returns>The generated data.</returns>
+        public static IEnumerable<object[]> GenerateOperatorEqualTestData() =>
+            FilterData((bool isEqual, bool _, bool _) => isEqual);
+
+        /// <summary>
+        /// Generates the data for testing <see cref="Dependency.operator !="/>.
+        /// </summary>
+        /// <returns>The generated data.</returns>
+        public static IEnumerable<object[]> GenerateOperatorNotEqualTestData() =>
+            FilterData((bool isEqual, bool _, bool _) => !isEqual);
+
+        /// <summary>
+        /// Generates the data for testing <see cref="Dependency.operator &lt;"/>.
+        /// </summary>
+        /// <returns>The generated data.</returns>
+        public static IEnumerable<object[]> GenerateOperatorLessThanTestData() =>
+            FilterData((bool _, bool isLessThan, bool _) => isLessThan);
+
+        /// <summary>
+        /// Generates the data for testing <see cref="Dependency.operator &lt;="/>.
+        /// </summary>
+        /// <returns>The generated data.</returns>
+        public static IEnumerable<object[]> GenerateOperatorLessThanOrEqualTestData() =>
+            FilterData((bool isEqual, bool isLessThan, bool _) => isLessThan || isEqual);
+
+        /// <summary>
+        /// Generates the data for testing <see cref="Dependency.operator &gt;"/>.
+        /// </summary>
+        /// <returns>The generated data.</returns>
+        public static IEnumerable<object[]> GenerateOperatorGreaterThanTestData() =>
+            FilterData((bool _, bool _, bool isGreaterThan) => isGreaterThan);
+
+        /// <summary>
+        /// Generates the data for testing <see cref="Dependency.operator &gt;="/>.
+        /// </summary>
+        /// <returns>The generated data.</returns>
+        public static IEnumerable<object[]> GenerateOperatorGreaterThanOrEqualTestData() =>
+            FilterData((bool isEqual, bool _, bool isGreaterThan) => isGreaterThan || isEqual);
 
         /// <summary>
         /// Tests that when <see cref="Dependency.Identifier"/> is called after construction, it returns the value
@@ -76,6 +149,145 @@ namespace NuGetTransitiveDependencyFinder.UnitTests.Output
 
             // Assert
             Assert.Equal(value, dependency.IsTransitive);
+        }
+
+        /// <summary>
+        /// Tests that when <see cref="Dependency.operator =="/> is called with different values, it returns the
+        /// expected value in each case.
+        /// </summary>
+        /// <param name="left">The left operand to compare.</param>
+        /// <param name="right">The right operand to compare.</param>
+        /// <param name="expected">The expected response.</param>
+        [Theory]
+        [MemberData(nameof(GenerateOperatorEqualTestData))]
+        public void OperatorEqual_CalledWithDifferentValues_ReturnsExpectedValues(
+            Dependency left,
+            Dependency right,
+            bool expected)
+        {
+            // Act
+            var result = left == right;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Tests that when <see cref="Dependency.operator !="/> is called with different values, it returns the
+        /// expected value in each case.
+        /// </summary>
+        /// <param name="left">The left operand to compare.</param>
+        /// <param name="right">The right operand to compare.</param>
+        /// <param name="expected">The expected response.</param>
+        [Theory]
+        [MemberData(nameof(GenerateOperatorNotEqualTestData))]
+        public void OperatorNotEqual_CalledWithDifferentValues_ReturnsExpectedValues(
+            Dependency left,
+            Dependency right,
+            bool expected)
+        {
+            // Act
+            var result = left != right;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Tests that when <see cref="Dependency.operator &lt;"/> is called with different values, it returns the
+        /// expected value in each case.
+        /// </summary>
+        /// <param name="left">The left operand to compare.</param>
+        /// <param name="right">The right operand to compare.</param>
+        /// <param name="expected">The expected response.</param>
+        [Theory]
+        [MemberData(nameof(GenerateOperatorLessThanTestData))]
+        public void OperatorLessThan_CalledWithDifferentValues_ReturnsExpectedValues(
+            Dependency left,
+            Dependency right,
+            bool expected)
+        {
+            // Act
+            var result = left < right;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Tests that when <see cref="Dependency.operator &lt;="/> is called with different values, it returns the
+        /// expected value in each case.
+        /// </summary>
+        /// <param name="left">The left operand to compare.</param>
+        /// <param name="right">The right operand to compare.</param>
+        /// <param name="expected">The expected response.</param>
+        [Theory]
+        [MemberData(nameof(GenerateOperatorLessThanOrEqualTestData))]
+        public void OperatorLessThanOrEqual_CalledWithDifferentValues_ReturnsExpectedValues(
+            Dependency left,
+            Dependency right,
+            bool expected)
+        {
+            // Act
+            var result = left <= right;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Tests that when <see cref="Dependency.operator &gt;"/> is called with different values, it returns the
+        /// expected value in each case.
+        /// </summary>
+        /// <param name="left">The left operand to compare.</param>
+        /// <param name="right">The right operand to compare.</param>
+        /// <param name="expected">The expected response.</param>
+        [Theory]
+        [MemberData(nameof(GenerateOperatorGreaterThanTestData))]
+        public void OperatorGreaterThan_CalledWithDifferentValues_ReturnsExpectedValues(
+            Dependency left,
+            Dependency right,
+            bool expected)
+        {
+            // Act
+            var result = left > right;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Tests that when <see cref="Dependency.operator &gt;="/> is called with different values, it returns the
+        /// expected value in each case.
+        /// </summary>
+        /// <param name="left">The left operand to compare.</param>
+        /// <param name="right">The right operand to compare.</param>
+        /// <param name="expected">The expected response.</param>
+        [Theory]
+        [MemberData(nameof(GenerateOperatorGreaterThanOrEqualTestData))]
+        public void OperatorGreaterThanOrEqual_CalledWithDifferentValues_ReturnsExpectedValues(
+            Dependency left,
+            Dependency right,
+            bool expected)
+        {
+            // Act
+            var result = left >= right;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Filters <see cref="OperatorTestData"/> using <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="predicate">The predicate with which to filter <see cref="OperatorTestData"/>.</param>
+        /// <returns>The filtered data.</returns>
+        private static IEnumerable<object[]> FilterData(Func<bool, bool, bool, bool> predicate)
+        {
+            foreach ((var left, var right, var isEqual, var isLessThan, var isGreaterThan) in OperatorTestData)
+            {
+                yield return new object[] { left, right, predicate(isEqual, isLessThan, isGreaterThan) };
+            }
         }
     }
 }
