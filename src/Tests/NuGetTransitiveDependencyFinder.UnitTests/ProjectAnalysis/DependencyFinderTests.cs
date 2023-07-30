824f171ea0adb55b1f4049c655107f18e5e4380c
Muiris Woulfe <mw.github@outlook.com>
2023-07-30 21:22:27 +0100

    Updating test method name to reflect incomplete nature

--- src/Tests/NuGetTransitiveDependencyFinder.UnitTests/ProjectAnalysis/DependencyFinderTests.cs
+++ src/Tests/NuGetTransitiveDependencyFinder.UnitTests/ProjectAnalysis/DependencyFinderTests.cs
@@ -218,7 +218,7 @@ public partial class DependencyFinderTests
     /// found, that project is returned.
     /// </summary>
     [AllCulturesFact]
-    public void Run_WithMatchingProject_ReturnsProject()
+    public void ToDo()
     {
         // Arrange
         const string projectOrSolutionPath = "C:\\project\\solution.sln";
